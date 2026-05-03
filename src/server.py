adb82127f783a4cef5d217554e3cd513bdf7351f
mhunt <57237495+mhuntno@users.noreply.github.com>
2026-05-03 18:42:07 +0200

    added args for port and local run (#37)

--- src/server.py
+++ src/server.py
@@ -1,3 +1,4 @@
+import argparse
 from concurrent import futures
 import random
 import time
@@ -21,13 +22,33 @@ class ProgressService(progress_pb2_grpc.ProgressServiceServicer):
         )
 
 def serve():
+    parser = argparse.ArgumentParser(
+        prog="skavl-anomaly-detection-module",
+        description="Anomaly detection in aerial images")
+
+    parser.add_argument("-p", "--port", help="Port to start tiler server with", default=50051)
+    parser.add_argument("-l", "--local", action="store_true",
+                        help="""Determines if all or only local connections should be accepted. 
+                                   If this argument is present, the servers IP will be 127.0.0.1, 
+                                   if this argument is omitted, the ip will be set to 0.0.0.0 meaning accept all connections""")
+
+    args = parser.parse_args()
+
     gdal.UseExceptions()
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=4))
     progress_pb2_grpc.add_ProgressServiceServicer_to_server(ProgressService(), server)
     tiler_pb2_grpc.add_TilerServiceServicer_to_server(TileServiceServicer(), server)
-    server.add_insecure_port("0.0.0.0:50051")
+
+    # Accepts connections only locally when running locally.
+    server_port = getattr(args, "port")
+    server_ip = ""
+    if getattr(args, "local", False):
+        server_ip = "127.0.0.1"
+    else:
+        server_ip = "0.0.0.0"
+    server.add_insecure_port(f"{server_ip}:{server_port}")
     server.start()
-    print("gRPC server listening on 0.0.0.0:50051")
+    print(f"gRPC server listening on {server_ip}:{server_port}")
     try:
         while True:
             time.sleep(3600)
